600cbe7b3da6c72e5dd0bbc898f3e7e97d668c88
bi0punk <systecinfo_cl@criptext.com>
2023-08-17 17:15:53 -0400

    add new features

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    selected_file_name = None
+    
     if request.method == 'POST':
         if 'csv_file' not in request.files:
             return redirect(request.url)
@@ -14,11 +16,13 @@ def index():
         if csv_file.filename == '':
             return redirect(request.url)
         
+        selected_file_name = csv_file.filename
+        
         if csv_file:
-            df = pd.read_csv(csv_file)
-            return render_template('index.html', data=df.to_html(classes='table table-striped table-bordered', index=False))
+            df = pd.read_csv(csv_file, skiprows=10)
+            return render_template('index.html', data=df.to_html(classes='table table-striped table-bordered', index=False), selected_file_name=selected_file_name)
     
-    return render_template('index.html', data=None)
+    return render_template('index.html', data=None, selected_file_name=selected_file_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
